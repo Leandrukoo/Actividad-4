d9604beaf18ecbf4c30484a8d9029fb656fcae71
estebanito0602 <estebanito0602@gmail.com>
2026-06-15 00:04:06 -0300

    Funcionando correctamente

--- mostrar.js
+++ mostrar.js
@@ -1,9 +1,9 @@
 const params = new URLSearchParams(window.location.search);
 
 document.getElementById("resultado").innerHTML = `
-  <p><strong>inputNombre:</strong> ${params.get("nombre")}</p>
-  <p><strong>inputEmail:</strong> ${params.get("email")}</p>
-  <p><strong>inputEdad:</strong> ${params.get("edad")}</p>
-  <p><strong>inputPais:</strong> ${params.get("pais")}</p>
-  <p><strong>inputTerminos:</strong> ${params.get("terminos")}</p>
+  <p><strong>Nombre:</strong> ${params.get("nombre")}</p>
+  <p><strong>Email:</strong> ${params.get("email")}</p>
+  <p><strong>Edad:</strong> ${params.get("edad")}</p>
+  <p><strong>País:</strong> ${params.get("pais")}</p>
+  <p><strong>Acepta términos:</strong> ${params.get("terminos")}</p>
 `;
